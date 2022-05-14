6bebb5c4170092e02a5f4541eeed8800c2bf007e
Franciel Cantanhede <franciel_cr@hotmail.com>
2022-05-14 20:23:19 -0300

    jquery

--- js/script.js
+++ js/script.js
@@ -1,3 +1,34 @@
+$(document).ready(function() {
+
+let slideAtual = 1;
+let listaSlides = ["banner-1", "banner-2", "banner-3"]
+
+setInterval(mudarSlide, 2000)
+
+function mudarSlide() {
+    console.log("slide atual:", slideAtual);
+
+    //remove o slide anterior
+    if (slideAtual > 0) {
+        $("#carrossel").removeClass(listaSlides[slideAtual - 1])
+    } else if (slideAtual == 0) {
+        $("#carrossel").removeClass(listaSlides[ (listaSlides.length) - 1 ])
+    }
+
+    //Adiciona o slide atual
+    $("#carrossel").addClass(listaSlides[slideAtual]);
+
+    //Indica o slide atual
+    slideAtual++
+    
+    if(slideAtual > (listaSlides.length) - 1) {
+        slideAtual = 0;
+    }
+
+}
+
+})
+
 function mostrarMenu() {
     let menu = document.getElementById("menu")
 
